7e75269a3fa93253a0c9a38da3d8dcb5b7995528
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-08-22 22:29:08 -0700

    added edit article

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -20,10 +20,16 @@ class ArticlesController < ApplicationController
   		redirect_to article_path(@article)
 	end
 
+	#look over this...bugged
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
 
 		redirect_to_articles_path
 	end
+
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
 end
